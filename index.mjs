81d8ae30e0b45fe155504957950fac7bfb7f85c4
danielChuks <danieluchechukwuchuks1@gmail.com>
2022-03-11 14:41:42 +0000

    including wager

--- index.mjs
+++ index.mjs
@@ -7,20 +7,25 @@ const startingBalance = stdlib.parseCurrency(100);
 
 const accAlice = await stdlib.newTestAccount(startingBalance)
 const accBob = await stdlib.newTestAccount(startingBalance);
- 
-  console.log('Hello, Alice and Bob!');
 
-  console.log('Launching...');
+const fmt = (x) => stdlib.formatCurrency(x,  4)
+const getBalance = async (who) => fmt(await stdlib.balanceOf(who));
+const beforeAlice = await(getBalance(accAlice))
+const beforeBob = await(getBalance(accBob))
+
+console.log('Welcome, Alice and Bob ');
+
+  console.log('Launching game...');
 
   //this lines of code represent the indivitual contracts of the participant....
-  const ctcAlice = accAlice.contract(backend);
-  const ctcBob = accBob.contract(backend, ctcAlice.getInfo());
+const ctcAlice = accAlice.contract(backend);
+const ctcBob = accBob.contract(backend, ctcAlice.getInfo());
 
   //asgnining players
   const HAND = ['Rock', 'Paper', 'Scissors'];
   const OUTCOME = ['Bob Wins', 'Draw', 'Alice Win'];
 
-  const Player = (who) => ({
+const Player = (who) => ({
       getHand: () => {
         const hand = Math.floor(Math.random() * HAND.length);
         console.log(`${who} chooses the hand: ${HAND [hand]}`);
@@ -38,10 +43,20 @@ const accBob = await stdlib.newTestAccount(startingBalance);
   await Promise.all([
     backend.Alice(ctcAlice, {
       ...Player('Alice'),
+      wager: stdlib.parseCurrency(50)
     }),
     backend.Bob(ctcBob, {
       ...Player('Bob'),
+     acceptWager: (amt) => {
+       console.log(`Bob accepts the  wager of ${fmt(amt)}.`)}
     }),
   ]);
+
+  const afterAlice = await getBalance(accAlice)
+  const afterBob = await getBalance(accBob)
+
+
+  console.log(`Alice went from ${beforeAlice} to ${afterAlice}`)
+  console.log(`Bob went from ${beforeBob} to ${afterBob}`)
 
   console.log('Goodbye, Alice and Bob!');
--- index.rsh
+++ index.rsh
@@ -5,32 +5,59 @@ const Player = {
 
   seeOutCome: Fun([UInt], Null)
 }
-
 //the player function indicate that both alice and bob has the features off player ..i.e they both have get hand and seeOutCome..
 export const main = Reach.App(() => {
   const Alice = Participant('Alice', {
     ...Player,
+    wager: UInt,
   });
   const Bob = Participant('Bob', {
-    ...Player
+    ...Player,
+    acceptWager: Fun([UInt], Null),
   });
 
   init();
 
 //Alice hand 
   Alice.only(() => {
+    const wager = declassify(interact.wager);
     const handAlice = declassify(interact.getHand());
   });
-  Alice.publish(handAlice);
+  Alice.publish(wager, handAlice).pay(wager);
   commit();
 
 
   Bob.only(() =>{
-    const handBob = declassify(interact.getHand());
+    interact.acceptWager(wager);
+    const handBob = declassify(interact.getHand())
   });
-  Bob.publish(handBob);
-  const outCome = (handAlice + (4 - handBob)) % 3;
+  Bob.publish(handBob).pay(wager);
+
+  const outcome = (handAlice + (4 - handBob)) % 3;
+  const            [forAlice, forBob] =
+    outcome == 2 ? [       2,      0] :
+    outcome == 0 ? [       0,      2] :
+    /* tie      */ [       1,      1];
+
+    transfer(forAlice * wager).to(Alice);
+    transfer(forBob   * wager).to(Bob);
+  
+  each([Alice, Bob], () =>{
+    interact.seeOutCome(outcome);
+  })
+
+ 
   commit();
 
 exit();
 });
+
+
+
+
+
+
+
+
+
+
